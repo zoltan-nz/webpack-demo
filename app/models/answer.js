963bdba428a874760ef3d5058f00fddfa8df77cb
Zoltan <zdebre@gmail.com>
2017-02-10 22:07:00 +1300

    Clean repo

--- app/models/answer.js
+++
@@ -1,9 +0,0 @@
-import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
-import { belongsTo, hasMany } from 'ember-data/relationships';
-
-export default Model.extend({
-  option: attr(),
-  question: belongsTo('question'),
-  users: hasMany('user')
-});
